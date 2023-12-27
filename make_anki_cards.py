4754652d793d342b6c63ff4ce34c27aa971ed05d
Max Halford <maxhalford25@gmail.com>
2023-12-27 21:53:23 +0100

    Initial commit

---
+++ make_anki_cards.py
@@ -0,0 +1,14 @@
+import datetime as dt
+import json
+import pathlib
+
+cards = pathlib.Path('cards.txt').open('w')
+
+# The Guardian
+
+date_urls = json.load(pathlib.Path('the_guardian/date_urls.json').open())
+for week in pathlib.Path('the_guardian').glob('*/*/*.json'):
+    date = dt.date(*map(int, (part.replace('.json', '') for part in week.parts[-3:])))
+    with open(week) as f:
+        for question, answer in json.load(f).items():
+            cards.write(f'{question}\t{answer}\t{date_urls[date.isoformat()]}\n')
